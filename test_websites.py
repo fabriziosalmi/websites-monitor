bef10de676834d3bb5078d62fa3806a92f8d91ac
fab <fabrizio.salmi@gmail.com>
2023-09-05 01:14:36 +0200

    Update test_websites.py

--- test_websites.py
+++ test_websites.py
@@ -1,35 +1,25 @@
 import requests
 import json
 from datetime import datetime
-import ssl
-import socket
-from OpenSSL import SSL
+import whois
 
-# Function to check SSL certificate
-def check_ssl_cert(host, port=443):
+# Function to check domain expiration
+def check_domain_expiration(domain):
     try:
-        # Make a request to fetch the SSL certificate
-        context = ssl.create_default_context()
-        conn = socket.create_connection((host, port))
-        sock = context.wrap_socket(conn, server_hostname=host)
-        cert = sock.getpeercert()
-        sock.close()
+        w = whois.whois(domain)
+        expiration_date = w.expiration_date
+        if isinstance(expiration_date, list):
+            expiration_date = expiration_date[0]
+        days_to_expire = (expiration_date - datetime.now()).days
 
-        # Extract the certificate expiration date
-        cert_expiry = datetime.strptime(cert['notAfter'], r"%b %d %H:%M:%S %Y %Z")
-        days_to_expire = (cert_expiry - datetime.utcnow()).days
-
-        if days_to_expire <= 0:
+        if days_to_expire < 15:
             return "🔴"
-        elif days_to_expire <= 30:
+        elif days_to_expire < 30:
             return "🟠"
         else:
             return "🟢"
-
-    except (ssl.SSLError, ssl.CertificateError, SSL.Error):
-        return "🔴"
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"An error occurred while checking domain expiration for {domain}: {e}")
         return "🔴"
 
 # List of websites to test
@@ -39,7 +29,7 @@ websites = [
 ]
 
 # Initialize Markdown report with table header
-report_md = "# Websites\n## PageSpeed and Security Header Report\n| Site | PageSpeed Score | Content-Security-Policy | Revealing Headers | SSL |\n|------|-----------------|--------------------------|------------------|-----|\n"
+report_md = "# Websites\n## PageSpeed and Security Header Report\n| Site | PageSpeed Score | Content-Security-Policy | Revealing Headers | Domain |\n|------|-----------------|--------------------------|------------------|--------|\n"
 
 for website in websites:
     # PageSpeed API
@@ -58,11 +48,11 @@ for website in websites:
     # Check for revealing headers
     revealing_status = "🟢" if not any(header in headers for header in ['Server', 'X-Powered-By', 'X-AspNet-Version']) else "🔴"
 
-    # Check SSL Certificate
-    ssl_status = check_ssl_cert(website)
+    # Check Domain Expiration
+    domain_status = check_domain_expiration(website)
 
     # Update Markdown report with table row data
-    report_md += f"| {website} | {pagespeed_score} | {csp_status} | {revealing_status} | {ssl_status} |\n"
+    report_md += f"| {website} | {pagespeed_score} | {csp_status} | {revealing_status} | {domain_status} |\n"
 
 # Get the current time and format it as a string
 current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
