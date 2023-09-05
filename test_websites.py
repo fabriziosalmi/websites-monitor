5ee19ac7d54138dd05fbe1897e7672863f4969f3
fab <fabrizio.salmi@gmail.com>
2023-09-05 22:23:44 +0200

    Update test_websites.py

--- test_websites.py
+++ test_websites.py
@@ -5,7 +5,6 @@ import whois
 import ssl
 import socket
 
-# Function to check domain expiration
 def check_domain_expiration(domain):
     try:
         w = whois.whois(domain)
@@ -24,7 +23,6 @@ def check_domain_expiration(domain):
         print(f"An error occurred while checking domain expiration for {domain}: {e}")
         return "🔴"
 
-# Function to check SSL certificate
 def check_ssl_cert(host, port=443):
     try:
         context = ssl.create_default_context()
@@ -45,63 +43,65 @@ def check_ssl_cert(host, port=443):
     except (ssl.SSLError, ssl.CertificateError):
         return "🔴"
 
-# Function to check for popular CDNs in headers
 def check_cdn(headers):
     cdn_headers = [
         ("Server", "cloudflare"),
         ("X-hello-human", "KeyCDN"),
         ("X-CDN", "stackpath"),
-        ("X-Cache", "Fastly")
+        ("X-Cache", "Fastly"),
+        ("Via", "1.1 varnish"),  # Varnish, also used by Fastly
+        ("Server", "ECS"),  # Amazon CloudFront
+        ("Server", "AkamaiGHost"),  # Akamai
+        ("Server", "nginx"),  # Could be a generic server, but commonly used in front of CDN
+        ("X-CDN-Geo", "ovh"),  # OVH CDN
+        ("Server", "CDN77-Turbo"),  # CDN77
+        ("X-IPLB-Instance", "Incap"),  # Incapsula
+        ("X-Powered-By", "Imperva")  # Imperva Incapsula
     ]
     for header, value in cdn_headers:
         if header in headers and value.lower() in headers[header].lower():
             return "🟢"
     return "🟠"
 
-# List of websites to test
-websites = [
-    'audiolibri.org',
-    'get.domainsblacklists.com'
-]
-
-# Initialize Markdown report with table header
-report_md = "# Websites monitor\n### Performances, headers, SSL/TLS, domain expiration and CDN enablement monitoring checks via Github action.\n| Site | Performances | CSP | Headers | SSL | Expiration | CDN |\n|------|-----------------|--------------------------|------------------|-----|--------|-----|\n"
-
-for website in websites:
-    # PageSpeed API
+def check_pagespeed(website):
     pagespeed_url = f"https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=https://{website}"
     pagespeed_response = requests.get(pagespeed_url)
     pagespeed_data = json.loads(pagespeed_response.text)
-    pagespeed_score = pagespeed_data["lighthouseResult"]["categories"]["performance"]["score"] * 100
+    return int(pagespeed_data["lighthouseResult"]["categories"]["performance"]["score"] * 100)
 
-    # Security Headers Check
-    security_response = requests.get(f"https://{website}")
-    headers = security_response.headers
-
-    # Determine CSP Status
+def check_security_headers(headers):
     csp_status = "🟢" if headers.get('Content-Security-Policy') else "🔴"
-
-    # Check for revealing headers
     revealing_status = "🟢" if not any(header in headers for header in ['Server', 'X-Powered-By', 'X-AspNet-Version']) else "🔴"
+    return csp_status, revealing_status
 
-    # Check SSL Certificate
-    ssl_status = check_ssl_cert(website)
+def check_reachability(website):
+    response = requests.get(f"https://{website}")
+    return "🟢" if response.status_code == 200 else "🔴"
 
-    # Check Domain Expiration
-    domain_status = check_domain_expiration(website)
+def write_report(websites):
+    report_md = "# Websites monitor\n### Performances, headers, SSL/TLS, domain expiration, reachability and CDN enablement monitoring checks via Github action.\n| Site | Reachability | Performances | CSP | Headers | SSL | Expiration | CDN |\n|------|--------------|-----------------|--------------------------|------------------|-----|--------|-----|\n"
+    
+    for website in websites:
+        reachability_status = check_reachability(website)
+        pagespeed_score = check_pagespeed(website)
+        security_response = requests.get(f"https://{website}")
+        headers = security_response.headers
+        csp_status, revealing_status = check_security_headers(headers)
+        ssl_status = check_ssl_cert(website)
+        domain_status = check_domain_expiration(website)
+        cdn_status = check_cdn(headers)
+        
+        report_md += f"| {website} | {reachability_status} | {pagespeed_score} | {csp_status} | {revealing_status} | {ssl_status} | {domain_status} | {cdn_status} |\n"
 
-    # Check CDN
-    cdn_status = check_cdn(headers)
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    report_md += f"\n---\nLast Updated: {current_time}"
+    
+    with open("README.md", "w") as f:
+        f.write(report_md)
 
-    # Update Markdown report with table row data
-    report_md += f"| {website} | {pagespeed_score} | {csp_status} | {revealing_status} | {ssl_status} | {domain_status} | {cdn_status} |\n"
-
-# Get the current time and format it as a string
-current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-# Add a "Last Updated" timestamp to the Markdown report
-report_md += f"\n---\nLast Updated: {current_time}"
-
-# Save report to a Markdown file
-with open("README.md", "w") as f:
-    f.write(report_md)
+if __name__ == "__main__":
+    websites = [
+        'audiolibri.org',
+        'get.domainsblacklists.com'
+    ]
+    write_report(websites)
