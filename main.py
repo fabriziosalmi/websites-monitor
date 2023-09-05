5b836e670adee63cc64452091a5733a880b2fec9
fab <fabrizio.salmi@gmail.com>
2023-09-06 01:09:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,85 @@
-import json
 from datetime import datetime
 from checks.check_pagespeed import check_pagespeed
 from checks.check_security_headers import check_security_headers
 from checks.check_ssl_cert import check_ssl_cert
 from checks.check_domain_expiration import check_domain_expiration
 from checks.check_cdn import check_cdn
+from checks.check_dns_blacklist import check_dns_blacklist
+from checks.check_alt_tags import check_alt_tags
+from checks.check_cors_headers import check_cors_headers
+from checks.check_cookie_flags import check_cookie_flags
+from checks.check_hsts import check_hsts
+from checks.check_open_graph_protocol import check_open_graph_protocol
+from checks.check_privacy_protected_whois import check_privacy_protected_whois
+from checks.check_privacy_exposure import check_privacy_exposure
+from checks.check_robot_txt import check_robot_txt
+from checks.check_sitemap import check_sitemap
+from checks.check_semantic_markup import check_semantic_markup
+from checks.check_subdomain_enumeration import check_subdomain_enumeration
+from checks.check_website_load_time import check_website_load_time
+from checks.check_xss_protection import check_xss_protection
 
 # Read websites from external file
 with open('websites.txt', 'r') as f:
     websites = [line.strip() for line in f.readlines()]
 
-# Initialize Markdown report with table header and project description
+# Initialize Markdown report
 report_md = "# Websites Monitor\n"
+
+# Add project description
 report_md += "## Project Description\n"
 report_md += "This project aims to continuously monitor various aspects of specified websites. "
-report_md += "It checks performances, security headers, SSL/TLS certificates, domain expiration, and CDN enablement. "
-report_md += "The GitHub action is scheduled to run once a day and updates this README with the latest results.\n"
+report_md += "It runs a variety of checks, ranging from performance to security considerations. "
+report_md += "The GitHub Action is scheduled to run once per day, updating this README with the latest results.\n"
+
+# Add usage instructions
 report_md += "\n## How to Use\n"
 report_md += "1. Fork this repository.\n"
 report_md += "2. Add the websites you want to monitor in the `websites.txt` file, one per line.\n"
 report_md += "3. Enable GitHub Actions if not already enabled.\n"
 report_md += "4. The README will be automatically updated with the latest check results once a day.\n"
 
+# Initialize the table
 report_md += "\n### Monitoring Checks\n"
 report_md += "| Check Type | " + " | ".join(websites) + " |\n"
 report_md += "|------------|" + "---|" * len(websites) + "\n"
 
-checks = ["Performances", "CSP", "Headers", "SSL", "Expiration", "CDN"]
-for check in checks:
-    report_md += f"| {check} | "
+# List of check functions and their human-readable names
+check_functions = [
+    ("Pagespeed Performances", check_pagespeed),
+    ("CSP", check_security_headers),
+    ("Headers", check_security_headers),
+    ("SSL Expiration", check_ssl_cert),
+    ("Domain Expiration", check_domain_expiration),
+    ("CDN", check_cdn),
+    ("DNS Blacklist", check_dns_blacklist),
+    ("Alt Tags", check_alt_tags),
+    ("CORS Headers", check_cors_headers),
+    ("Cookie Flags", check_cookie_flags),
+    ("HSTS", check_hsts),
+    ("Open Graph Protocol", check_open_graph_protocol),
+    ("Privacy-Protected Whois", check_privacy_protected_whois),
+    ("Privacy Exposure", check_privacy_exposure),
+    ("Robots.txt", check_robot_txt),
+    ("Sitemap", check_sitemap),
+    ("Semantic Markup", check_semantic_markup),
+    ("Subdomain Enumeration", check_subdomain_enumeration),
+    ("Website Load Time", check_website_load_time),
+    ("XSS Protection", check_xss_protection),
+]
+
+# Populate the table with check results
+for check_name, check_func in check_functions:
+    report_md += f"| {check_name} | "
     for website in websites:
-        if check == "Performances":
-            report_md += f"{check_pagespeed(website)} | "
-        elif check == "CSP" or check == "Headers":
-            csp_status, revealing_status = check_security_headers(website)
-            report_md += f"{csp_status if check == 'CSP' else revealing_status} | "
-        elif check == "SSL":
-            report_md += f"{check_ssl_cert(website)} | "
-        elif check == "Expiration":
-            report_md += f"{check_domain_expiration(website)} | "
-        elif check == "CDN":
-            report_md += f"{check_cdn(website)} | "
+        result = check_func(website)
+        report_md += f"{result} | "
     report_md += "\n"
 
-# Get the current time and format it as a string
+# Timestamp
 current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+report_md += f"\n---\nLast Updated: {current_time}\n"
 
-# Add a "Last Updated" timestamp to the Markdown report
-report_md += f"\n---\nLast Updated: {current_time}"
-
-# Save report to a Markdown file
+# Write the Markdown report to a file
 with open("README.md", "w") as f:
     f.write(report_md)
