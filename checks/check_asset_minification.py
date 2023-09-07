861b69708db8a900ef5f6cae9c3564969a9c8153
fab <fabrizio.salmi@gmail.com>
2023-09-07 15:04:16 +0200

    Update check_asset_minification.py

--- checks/check_asset_minification.py
+++ checks/check_asset_minification.py
@@ -1,7 +1,7 @@
 import requests
 
-def check_asset_minification(css_js_links):
-    for link in css_js_links:
+def check_asset_minification(website):
+    for link in website:
         content = requests.get(link).text
         if len(content) == len(content.strip()):
             return "🔴"
