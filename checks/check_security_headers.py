51439511a6312b60cdcafd3118a82ebc27be8902
fab <fabrizio.salmi@gmail.com>
2023-09-07 09:41:12 +0200

    Update check_security_headers.py

--- checks/check_security_headers.py
+++ checks/check_security_headers.py
@@ -1,28 +1,43 @@
 import requests
 
 def check_security_headers(website):
-    result = {}
+    # List of recommended headers and their respective ideal values or indicators
+    recommended_headers = {
+        'Content-Security-Policy': None, 
+        'Strict-Transport-Security': None,
+        'X-Content-Type-Options': "nosniff",
+        'X-Frame-Options': None,
+        'X-XSS-Protection': "1; mode=block",
+        'Referrer-Policy': None
+    }
+
     try:
         response = requests.get(f"https://{website}")
         headers = response.headers
+        
+        headers_found = 0
+        headers_partially_implemented = 0
 
-        # Check for Content Security Policy (CSP)
-        csp = headers.get('Content-Security-Policy', None)
-        if csp:
-            result['CSP'] = "🟢"
-        else:
-            result['CSP'] = "🔴"
+        # Check each recommended header
+        for header, ideal_value in recommended_headers.items():
+            value = headers.get(header)
+            if value:
+                headers_found += 1
+                if ideal_value and value != ideal_value:
+                    headers_partially_implemented += 1
 
         # Check for revealing headers like 'Server', 'X-Powered-By', etc.
         revealing_headers = ['Server', 'X-Powered-By', 'X-AspNet-Version']
         if any(header in headers for header in revealing_headers):
-            result['Revealing-Headers'] = "🔴"
+            headers_partially_implemented += 1
+        
+        if headers_found == len(recommended_headers) and headers_partially_implemented == 0:
+            return "🟢"  # Green: All headers properly implemented
+        elif headers_found > 0 and headers_partially_implemented > 0:
+            return "🟠"  # Orange: Headers are present but not all are ideally implemented
         else:
-            result['Revealing-Headers'] = "🟢"
+            return "🔴"  # Red: Not properly implemented
 
     except Exception as e:
         print(f"An error occurred while checking security headers for {website}: {e}")
-        result['CSP'] = "🔴"
-        result['Revealing-Headers'] = "🔴"
-
-    return result
+        return "🔴"  # Red: Error or headers not implemented
