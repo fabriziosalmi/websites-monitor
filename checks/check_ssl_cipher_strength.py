16dc01cf91ce3dbcc05e8b6180000db228e4e5a9
fab <fabrizio.salmi@gmail.com>
2023-09-07 13:42:32 +0200

    Create check_ssl_cipher_strength.py

---
+++ checks/check_ssl_cipher_strength.py
@@ -0,0 +1,36 @@
+import requests
+
+def check_ssl_cipher_strength(website):
+    """
+    Check the strength of the SSL/TLS cipher suite of the website.
+    
+    Args:
+    - website (str): URL of the website to be checked.
+    
+    Returns:
+    - str: "🟢" if the cipher strength is strong,
+           "🟠" if the cipher strength is moderate,
+           "🔴" if the cipher strength is weak,
+           "⚪" for any errors.
+    """
+
+    try:
+        response = requests.get(website)
+        cipher = response.raw.connection.socket.get_cipher()[0]
+
+        # Depending on your security requirements, adjust these
+        strong_ciphers = ['ECDHE-RSA-AES128-GCM-SHA256', 'ECDHE-RSA-AES256-GCM-SHA384']
+        moderate_ciphers = ['ECDHE-RSA-AES128-SHA', 'ECDHE-RSA-AES256-SHA']
+        
+        if cipher in strong_ciphers:
+            return "🟢"
+        elif cipher in moderate_ciphers:
+            return "🟠"
+        else:
+            return "🔴"
+
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return "⚪"
+
+print(check_ssl_cipher_strength("https://example.com"))
