4c683ba69375e3b11e1584e36cd519f88af8f7ec
fab <fabrizio.salmi@gmail.com>
2023-09-07 09:44:38 +0200

    Update check_pagespeed.py

--- checks/check_pagespeed.py
+++ checks/check_pagespeed.py
@@ -2,9 +2,20 @@ import requests
 import json
 
 def check_pagespeed(website):
-    # Your code to check PageSpeed
-    pagespeed_url = f"https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=https://{website}"
-    pagespeed_response = requests.get(pagespeed_url)
-    pagespeed_data = json.loads(pagespeed_response.text)
-    pagespeed_score = pagespeed_data["lighthouseResult"]["categories"]["performance"]["score"] * 100
-    return pagespeed_score
+    try:
+        pagespeed_url = f"https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=https://{website}"
+        pagespeed_response = requests.get(pagespeed_url)
+        
+        # Check if the API responded with a valid response
+        if pagespeed_response.status_code != 200:
+            print(f"Error occurred while fetching PageSpeed for {website}. Status Code: {pagespeed_response.status_code}")
+            return 0  # Returning 0 as an indication of an API error
+        
+        pagespeed_data = json.loads(pagespeed_response.text)
+        pagespeed_score = pagespeed_data["lighthouseResult"]["categories"]["performance"]["score"] * 100
+        
+        return pagespeed_score
+        
+    except Exception as e:
+        print(f"An error occurred while checking PageSpeed for {website}: {e}")
+        return 0  # Returning 0 as an indication of a processing error
