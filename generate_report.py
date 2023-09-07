b1dc03b26b2d74abca2822b02a14ed3625c2b3f5
fab <fabrizio.salmi@gmail.com>
2023-09-07 10:51:36 +0200

    Update generate_report.py

--- generate_report.py
+++ generate_report.py
@@ -2,17 +2,16 @@ from github import Github
 import os
 import subprocess
 import datetime
+import traceback
 
 def generate_pdf_report_from_readme():
-    # Use pandoc to convert README.md to PDF, excluding the introduction
     output_file = "report.pdf"
     with open("README.md", "r") as file:
         content = file.readlines()
 
-    # Filter out the introduction or any other unwanted lines
     start_index = None
     for idx, line in enumerate(content):
-        if "| Website | Result |" in line:  # Assuming this is the start of your table
+        if "| Website | Result |" in line:
             start_index = idx
             break
 
@@ -25,24 +24,27 @@ def generate_pdf_report_from_readme():
     with open("temp.md", "w") as temp_file:
         temp_file.write(temp_md)
 
-    # Convert to PDF
     cmd = f"pandoc temp.md -o {output_file}"
     subprocess.call(cmd, shell=True)
-    os.remove("temp.md")  # Cleanup temporary markdown file
+    os.remove("temp.md")
 
     return output_file
 
 def create_github_release(pdf_path):
-    # Authenticate with GitHub
     g = Github(os.environ["GITHUB_TOKEN"])
-    repo = g.get_user().get_repo("websites-monitor")
+    repo = g.get_repo("fabriziosalmi/websites-monitor")  # Specifying repo in this manner
 
-    # Create a new release
     release = repo.create_git_release(tag="latest", name="Report.PDF", message="Latest monitoring report.", draft=False, prerelease=False)
-
-    # Attach PDF to the release
     release.upload_asset(pdf_path)
 
 def main():
-    pdf_path = generate_pdf_report_from_readme()
-    create_github_release(pdf_path)
+    try:
+        pdf_path = generate_pdf_report_from_readme()
+        if pdf_path:
+            create_github_release(pdf_path)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        traceback.print_exc()
+
+if __name__ == "__main__":
+    main()
