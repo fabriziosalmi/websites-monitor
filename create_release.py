2ccbdfacca4037e5bf1938d92d3cc1f97f930ff4
fab <fabrizio.salmi@gmail.com>
2023-09-07 11:19:12 +0200

    Update create_release.py

--- create_release.py
+++ create_release.py
@@ -1,42 +1,24 @@
 from github import Github
 import os
 import subprocess
-import datetime
+import requests
 
 def generate_pdf_report_from_readme():
-    # Use requests to get the raw content of README.md from the repo
-    import requests
-
+    # Get the raw content of README.md from the repo
     url = "https://raw.githubusercontent.com/fabriziosalmi/websites-monitor/main/README.md"
     response = requests.get(url)
+    
     if response.status_code != 200:
         print(f"Failed to fetch README.md. Status Code: {response.status_code}")
         return
 
-    content = response.text.split("\n")
-    output_file = "report.pdf"
+    content = response.text
+    output_file = "latest.pdf"
 
-    # Find start and end indices
-    start_index = None
-    end_index = None
-
-    for idx, line in enumerate(content):
-        if "Monitoring Checks" in line:  # Start of the desired content
-            start_index = idx
-        if "Last Update:" in line:  # End of the desired content
-            end_index = idx
-            break
-
-    if start_index is None or end_index is None:
-        print("Desired content not found in README.md")
-        return
-
-    desired_content = content[start_index:end_index+1]
-    temp_md = "\n".join(desired_content)
     with open("temp.md", "w") as temp_file:
-        temp_file.write(temp_md)
+        temp_file.write(content)
 
-    # Convert to PDF
+    # Convert the temporary markdown file to PDF
     cmd = f"pandoc temp.md -o {output_file}"
     subprocess.call(cmd, shell=True)
     os.remove("temp.md")  # Cleanup temporary markdown file
@@ -49,7 +31,7 @@ def create_github_release(pdf_path):
     repo = g.get_user().get_repo("websites-monitor")
 
     # Create a new release
-    release = repo.create_git_release(tag="latest", name="Report.PDF", message="Latest monitoring report.", draft=False, prerelease=False)
+    release = repo.create_git_release(tag="latest", name="Latest Report", message="Latest monitoring report.", draft=False, prerelease=False)
 
     # Attach PDF to the release
     release.upload_asset(pdf_path)
