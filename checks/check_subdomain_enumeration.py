478a3b79f567d48629adb9334bcf5b7b95c13d33
fab <fabrizio.salmi@gmail.com>
2023-09-08 22:14:29 +0200

    Update check_subdomain_enumeration.py

--- checks/check_subdomain_enumeration.py
+++ checks/check_subdomain_enumeration.py
@@ -1,12 +1,35 @@
 import requests
 
-def check_subdomain_enumeration(website):
-    subdomains = ["www", "api", "dev"]
-    for sub in subdomains:
-        try:
-            response = requests.get(f"https://{sub}.{website}")
-            if response.status_code == 200:
-                return "🔴"
-        except Exception as e:
-            continue
-    return "🟢"
+def check_subdomain_enumeration(website: str) -> (str, list):
+    """
+    Check for the existence of common subdomains for a given website.
+    
+    Args:
+    - website (str): The main domain of the website to be checked.
+    
+    Returns:
+    - tuple: A status symbol and a list of discovered subdomains.
+             "🟢" if no subdomains were discovered,
+             "🔴" if any subdomains were found,
+             "⚪" for unexpected errors.
+    """
+    SUBDOMAINS = ["www", "api", "dev"]
+    discovered_subdomains = []
+    
+    with requests.Session() as session:
+        for sub in SUBDOMAINS:
+            try:
+                response = session.get(f"https://{sub}.{website}", timeout=5)
+                if response.status_code == 200:
+                    discovered_subdomains.append(sub)
+            except (requests.RequestException, Exception):
+                continue
+
+    if discovered_subdomains:
+        return "🔴", discovered_subdomains
+    return "🟢", []
+
+# Usage:
+status, subdomains_found = check_subdomain_enumeration("example.com")
+print(status)
+print(subdomains_found)
