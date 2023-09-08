dd551134a3efbaed157b637255e8656939acb0fa
fab <fabrizio.salmi@gmail.com>
2023-09-08 22:09:51 +0200

    Update check_security_headers.py

--- checks/check_security_headers.py
+++ checks/check_security_headers.py
@@ -18,7 +18,7 @@ def check_security_headers(website):
         headers_found = 0
         headers_partially_implemented = 0
 
-        # Check each recommended header
+        # Check each recommended header for presence and correct value.
         for header, ideal_value in recommended_headers.items():
             value = headers.get(header)
             if value:
@@ -26,18 +26,24 @@ def check_security_headers(website):
                 if ideal_value and value != ideal_value:
                     headers_partially_implemented += 1
 
-        # Check for revealing headers like 'Server', 'X-Powered-By', etc.
-        revealing_headers = ['Server', 'X-Powered-By', 'X-AspNet-Version']
-        if any(header in headers for header in revealing_headers):
+        # Check for revealing headers which might disclose sensitive information.
+        revealing_headers = set(['Server', 'X-Powered-By', 'X-AspNet-Version'])
+        if revealing_headers.intersection(headers):
             headers_partially_implemented += 1
         
-        if headers_found == len(recommended_headers) and headers_partially_implemented == 0:
-            return "🟢"  # Green: All headers properly implemented
-        elif headers_found > 0 and headers_partially_implemented > 0:
-            return "🟠"  # Orange: Headers are present but not all are ideally implemented
+        # Decision-making based on the checks.
+        if headers_found == len(recommended_headers):
+            if headers_partially_implemented == 0:
+                return "🟢"  # Green: All headers properly implemented
+            else:
+                return "🟠"  # Orange: Headers are present but not all are ideally implemented
         else:
-            return "🔴"  # Red: Not properly implemented
+            return "🔴"  # Red: Some headers are missing
+
+    except requests.RequestException:
+        print(f"Network-related error occurred while checking security headers for {website}.")
+        return "🔴"  # Red: Error occurred
 
     except Exception as e:
-        print(f"An error occurred while checking security headers for {website}: {e}")
-        return "🔴"  # Red: Error or headers not implemented
+        print(f"An unexpected error occurred while checking security headers for {website}: {e}")
+        return "🔴"  # Red: Error occurred
