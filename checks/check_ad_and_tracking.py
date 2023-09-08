8293dbdf05f75b1ead93f28c9ea11bc7c5cc81ae
fab <fabrizio.salmi@gmail.com>
2023-09-08 21:30:17 +0200

    Update check_ad_and_tracking.py

--- checks/check_ad_and_tracking.py
+++ checks/check_ad_and_tracking.py
@@ -1,11 +1,25 @@
 import requests
 
 def check_ad_and_tracking(website):
+    """
+    Check if the website is using Google Analytics and/or AdsbyGoogle.
+
+    Returns:
+        "🔴" if both are present
+        "🟠" if only Google Analytics is present
+        "🟢" if neither are present
+    """
+
     try:
-        response = requests.get(f"https://{website}")
-        if "google-analytics" in response.text and "adsbygoogle" in response.text:
+        response = requests.get(f"https://{website}", timeout=10)  # Adding a timeout of 10 seconds
+
+        # Determine the status based on the presence of the scripts
+        has_google_analytics = "google-analytics" in response.text
+        has_adsbygoogle = "adsbygoogle" in response.text
+
+        if has_google_analytics and has_adsbygoogle:
             return "🔴"
-        elif "google-analytics" in response.text:
+        elif has_google_analytics:
             return "🟠"
         else:
             return "🟢"
