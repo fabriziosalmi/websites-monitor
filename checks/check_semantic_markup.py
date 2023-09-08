dedb22549f4d43d07fa213dd115daf956a881de5
fab <fabrizio.salmi@gmail.com>
2023-09-08 22:10:32 +0200

    Update check_semantic_markup.py

--- checks/check_semantic_markup.py
+++ checks/check_semantic_markup.py
@@ -1,9 +1,24 @@
 from bs4 import BeautifulSoup
 
 def check_semantic_markup(html_content):
+    """
+    Check if the provided HTML content contains semantic markup 
+    in the form of JSON-LD or Microdata.
+
+    Args:
+        html_content (str): The HTML content to be checked.
+
+    Returns:
+        str: "🟢" if semantic markup is found, "🔴" otherwise.
+    """
     soup = BeautifulSoup(html_content, 'html.parser')
-    json_ld = soup.find_all(type="application/ld+json")
-    microdata = soup.find_all(attrs={"itemscope": True})
+    
+    # Check for JSON-LD semantic markup
+    json_ld = soup.find(type="application/ld+json")
+    
+    # Check for Microdata semantic markup
+    microdata = soup.find(attrs={"itemscope": True})
+    
     if json_ld or microdata:
         return "🟢"
     return "🔴"
