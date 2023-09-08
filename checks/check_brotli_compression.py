d9dd593ebd440be312a5c246993d6d9b6e14f79f
fab <fabrizio.salmi@gmail.com>
2023-09-08 21:35:38 +0200

    Update check_brotli_compression.py

--- checks/check_brotli_compression.py
+++ checks/check_brotli_compression.py
@@ -1,8 +1,18 @@
 import requests
 
 def check_brotli_compression(website):
-    response = requests.get(f"https://{website}")
-    if 'br' in response.headers.get('Content-Encoding', ''):
-        return "🟢"
-    else:
+    headers = {
+        "Accept-Encoding": "gzip, deflate, br",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    }
+    
+    try:
+        response = requests.get(f"https://{website}", headers=headers, timeout=10)
+        
+        if 'br' in response.headers.get('Content-Encoding', ''):
+            return "🟢"
+        else:
+            return "🔴"
+    except Exception as e:
+        print(f"An error occurred while checking Brotli compression for {website}: {e}")
         return "🔴"
