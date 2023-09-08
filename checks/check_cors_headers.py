5e2f23123fade0d3614dbf17cb30fb596bbc4e9f
fab <fabrizio.salmi@gmail.com>
2023-09-08 21:45:31 +0200

    Update check_cors_headers.py

--- checks/check_cors_headers.py
+++ checks/check_cors_headers.py
@@ -1,13 +1,32 @@
 import requests
 
 def check_cors_headers(website):
+    """
+    Checks the CORS policy of the given website.
+    
+    Args:
+    - website (str): The website URL to check.
+    
+    Returns:
+    - str: "🟢" if the CORS policy is not a wildcard,
+           "🔴" if the CORS policy is a wildcard or an error occurred during the check,
+           "⚪" if an unexpected error occurred.
+    """
     try:
         response = requests.options(f"https://{website}")
         cors_header = response.headers.get('Access-Control-Allow-Origin', 'None')
-        if cors_header != '*':
-            return "🟢"
-        else:
+
+        # Handling multiple origins
+        allowed_origins = [origin.strip() for origin in cors_header.split(",")]
+
+        if '*' in allowed_origins:
+            print(f"{website} has a wildcard CORS policy.")
             return "🔴"
-    except Exception as e:
-        print(f"An error occurred while checking CORS headers for {website}: {e}")
+        return "🟢"
+
+    except requests.exceptions.RequestException as req_err:
+        print(f"Request error occurred while checking CORS headers for {website}: {req_err}")
         return "🔴"
+    except Exception as e:
+        print(f"Unexpected error occurred while checking CORS headers for {website}: {e}")
+        return "⚪"
