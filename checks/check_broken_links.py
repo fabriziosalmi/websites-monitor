55b2216e2968d65263b4117b3379e97ea248a7b6
fab <fabrizio.salmi@gmail.com>
2023-09-08 21:34:45 +0200

    Update check_broken_links.py

--- checks/check_broken_links.py
+++ checks/check_broken_links.py
@@ -7,14 +7,29 @@ def check_broken_links(website):
         soup = BeautifulSoup(response.text, 'html.parser')
         links = soup.find_all('a')
         
+        checked_links = set()  # To avoid checking same URL twice
+        broken_link_count = 0
+        total_links = 0
+
         for link in links:
             url = link.get('href')
+            if not url or url.startswith('#') or url.startswith('javascript:'):
+                continue
             if url.startswith('/'):
                 url = f"https://{website}{url}"
-            link_response = requests.get(url)
-            if link_response.status_code == 404:
-                return "🔴"
-        return "🟢"
+            if url not in checked_links:
+                link_response = requests.get(url, allow_redirects=True, timeout=10)
+                checked_links.add(url)
+                if 400 <= link_response.status_code < 600:  # Checking for client and server errors
+                    broken_link_count += 1
+                total_links += 1
+
+        if broken_link_count == 0:
+            return "🟢"
+        elif broken_link_count < total_links:
+            return "🟠"
+        else:
+            return "🔴"
     except Exception as e:
         print(f"An error occurred while checking broken links for {website}: {e}")
         return "🔴"
