af9eaa38207930bfd702dbc4ea5135f75d8de1aa
fab <fabrizio.salmi@gmail.com>
2023-09-08 21:41:46 +0200

    Update check_cookie_flags.py

--- checks/check_cookie_flags.py
+++ checks/check_cookie_flags.py
@@ -3,9 +3,24 @@ import requests
 def check_cookie_flags(website):
     try:
         response = requests.get(f"https://{website}")
-        cookie_flags = response.headers.get('Set-Cookie', '')
-        if 'Secure' in cookie_flags and 'HttpOnly' in cookie_flags:
+        set_cookie_headers = response.headers.getlist('Set-Cookie')
+        
+        all_secure_http_only = True
+        any_secure_http_only = False
+        
+        for cookie_header in set_cookie_headers:
+            if 'Secure' in cookie_header and 'HttpOnly' in cookie_header:
+                any_secure_http_only = True
+            else:
+                all_secure_http_only = False
+        
+        # All cookies have Secure and HttpOnly flags
+        if all_secure_http_only:
             return "🟢"
+        # Some cookies have Secure and HttpOnly flags, but not all
+        elif any_secure_http_only:
+            return "🟠"
+        # No cookies have both Secure and HttpOnly flags
         else:
             return "🔴"
     except Exception as e:
