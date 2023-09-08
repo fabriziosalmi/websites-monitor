d31f34ef005d7dda610c4527e62476ee723d23fd
fab <fabrizio.salmi@gmail.com>
2023-09-08 21:32:35 +0200

    Update check_amp_compatibility.py

--- checks/check_amp_compatibility.py
+++ checks/check_amp_compatibility.py
@@ -1,8 +1,25 @@
 from bs4 import BeautifulSoup
 
 def check_amp_compatibility(html_content):
-    soup = BeautifulSoup(html_content, 'html.parser')
-    amp_html = soup.find_all("html", attrs={"⚡": ""}) or soup.find_all("html", attrs={"amp": ""})
-    if amp_html:
-        return "🟢"
-    return "🔴"
+    """
+    Check if the provided HTML content has AMP compatibility.
+
+    Returns:
+        "🟢" if AMP compatible
+        "🔴" if not AMP compatible
+    """
+    
+    try:
+        soup = BeautifulSoup(html_content, 'html.parser')
+        
+        # Checking for both standard AMP tags: ⚡ and amp
+        amp_html = soup.find_all("html", attrs={"⚡": ""}) or soup.find_all("html", attrs={"amp": ""})
+        
+        if amp_html:
+            return "🟢"
+        else:
+            return "🔴"
+    except Exception as e:
+        print(f"An error occurred while checking AMP compatibility: {e}")
+        # If an error occurs, we assume it's not AMP compatible for safety
+        return "🔴"
