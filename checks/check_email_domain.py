bb72d7435bf241778e737de27768e9a3d7679c7d
fab <fabrizio.salmi@gmail.com>
2023-09-08 21:54:21 +0200

    Update check_email_domain.py

--- checks/check_email_domain.py
+++ checks/check_email_domain.py
@@ -1,12 +1,30 @@
 import dns.resolver
 
 def check_email_domain(email_domain):
+    """
+    Check if an email domain has an SPF record.
+
+    Args:
+    - email_domain (str): The domain of the email to be checked.
+
+    Returns:
+    - str: "🟢" if an SPF record is found,
+           "🔴" if no SPF record is found,
+           "⚪" for any other errors or issues.
+    """
+
     try:
         answers = dns.resolver.resolve(email_domain, 'TXT')
-        for rdata in answers:
-            if "v=spf1" in str(rdata):
-                return "🟢"
+        if any("v=spf1" in str(rdata) for rdata in answers):
+            return "🟢"
+        else:
+            return "🔴"
+    except dns.resolver.NXDOMAIN:
+        print(f"{email_domain} does not exist.")
+        return "🔴"
+    except dns.resolver.NoAnswer:
+        print(f"{email_domain} does not have a TXT record.")
         return "🔴"
     except Exception as e:
         print(f"An error occurred while checking email domain for {email_domain}: {e}")
-        return "🔴"
+        return "⚪"
