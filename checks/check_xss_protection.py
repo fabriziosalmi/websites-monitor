c3705c29a27a3e9d725331ccdda25b8246887abd
fab <fabrizio.salmi@gmail.com>
2023-09-08 22:18:28 +0200

    Update check_xss_protection.py

--- checks/check_xss_protection.py
+++ checks/check_xss_protection.py
@@ -1,12 +1,32 @@
 import requests
+from typing import Optional
 
-def check_xss_protection(website):
+def check_xss_protection(website: str, timeout_seconds: Optional[int] = 5) -> str:
+    """
+    Check if the X-XSS-Protection header is present in the HTTP response headers of a website.
+
+    Args:
+    - website (str): The URL of the website to be checked.
+    - timeout_seconds (int, optional): Timeout for the HTTP request in seconds. Default is 5 seconds.
+
+    Returns:
+    - str: "🟢" if X-XSS-Protection header is present, "🔴" otherwise, "⚪" for any errors.
+    """
     try:
-        response = requests.get(f"https://{website}")
-        if 'X-XSS-Protection' in response.headers:
-            return "🟢"
+        response = requests.get(f"https://{website}", timeout=timeout_seconds)
+        
+        # Check if the response status code is in the 2xx range
+        if 200 <= response.status_code < 300:
+            if 'X-XSS-Protection' in response.headers:
+                return "🟢"
+            else:
+                return "🔴"
         else:
-            return "🔴"
+            print(f"Received non-success HTTP status code: {response.status_code}")
+            return "⚪"
+    except requests.Timeout:
+        print(f"Timeout occurred while checking XSS protection for {website}.")
+        return "⚪"
     except Exception as e:
         print(f"An error occurred while checking XSS protection for {website}: {e}")
-        return "🔴"
+        return "⚪"
