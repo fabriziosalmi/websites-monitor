6490979286b09f14bfed37f7d458088af88ead9f
fab <fabrizio.salmi@gmail.com>
2023-09-08 21:36:50 +0200

    Update check_cdn.py

--- checks/check_cdn.py
+++ checks/check_cdn.py
@@ -1,14 +1,24 @@
 def check_cdn(headers):
-    cdn_headers = [
-        ("Server", "cloudflare"),
-        ("Server", "KeyCDN"),
-        ("X-CDN", "stackpath"),
-        ("X-Cache", "Fastly"),
-        ("X-CDN", "Akamai"),
-        ("X-Powered-By", "CDN77"),
-        ("X-hello-human", "KeyCDN")
-    ]
-    for header, value in cdn_headers:
-        if header in headers and value.lower() in headers[header].lower():
-            return "🟢"
-    return "🟠"
+    """
+    Checks the provided HTTP headers for signs of popular Content Delivery Networks (CDNs).
+    
+    :param headers: Dictionary of HTTP headers.
+    :return: "🟢" if a known CDN is detected, "🟠" if unsure, and "🔴" if no CDN is detected.
+    """
+    
+    cdn_headers = {
+        "Server": ["cloudflare", "KeyCDN"],
+        "X-CDN": ["stackpath", "Akamai"],
+        "X-Cache": ["Fastly"],
+        "X-Powered-By": ["CDN77"],
+        "X-hello-human": ["KeyCDN"]
+    }
+
+    for header, possible_values in cdn_headers.items():
+        if header in headers:
+            for value in possible_values:
+                if value.lower() in headers[header].lower():
+                    return "🟢"
+    
+    # If the function hasn't returned by now, it means no known CDN was detected.
+    return "🔴"
